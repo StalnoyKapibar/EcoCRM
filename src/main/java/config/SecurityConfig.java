46d2146a48d49cd4f085bcfa6fa9c16edd7dc7d2
Marfall <marinefalling@gmail.com>
2019-08-18 14:48:31 +0300

    config_start

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by whitenoise on 18.08.19.
+ */
+public class SecurityConfig extends WebMvcConfigurerAdapter {
+
+
+
+}

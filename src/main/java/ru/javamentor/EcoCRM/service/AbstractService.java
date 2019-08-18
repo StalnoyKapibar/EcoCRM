edb51fbe18c8bc2ff2b991d84de661316a1a32a7
Alexey Fomichev <hang232@yandex.ru>
2019-08-18 23:01:46 +0300

    added AbstractDao and AbstractService

--- src/main/java/ru/javamentor/EcoCRM/service/AbstractService.java
+++ src/main/java/ru/javamentor/EcoCRM/service/AbstractService.java
@@ -1,4 +1,30 @@
 package ru.javamentor.EcoCRM.service;
 
-public class AbstractService<T> {
+import ru.javamentor.EcoCRM.dao.AbstractDao;
+
+import java.util.List;
+
+public abstract class AbstractService<T> {
+    private AbstractDao<T> abstractDao;
+
+    public void delete(T t) {
+        abstractDao.delete(t);
+    }
+
+    public void insert(T t) {
+        abstractDao.insert(t);
+    }
+
+    public void update(T t) {
+        abstractDao.update(t);
+    }
+
+    public T get(long id) {
+        return abstractDao.get(id);
+    }
+
+    public List<T> getAll() {
+        return abstractDao.getAll();
+    }
+
 }

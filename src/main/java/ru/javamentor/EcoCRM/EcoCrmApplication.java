4e9f3df7af2bd627da48ec100f065c50b38a4dce
Marfall <marinefalling@gmail.com>
2019-08-18 12:43:49 +0300

    initial boot project

---
+++ src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
@@ -0,0 +1,13 @@
+package ru.javamentor.EcoCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/javamentor/EcoCRM/EcoCrmApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.javamentor.EcoCRM;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcoCrmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

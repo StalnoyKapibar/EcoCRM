c5dc90049499bfa78d71250a5fef6b16569d02dd
Aytalina Malysheva <aita.m@bk.ru>
2019-08-19 10:45:16 +0300

    correct

--- src/main/java/ru/javamentor/EcoCRM/service/ProjectService.java
+++ src/main/java/ru/javamentor/EcoCRM/service/ProjectService.java
@@ -1,6 +1,5 @@
 package ru.javamentor.EcoCRM.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javamentor.EcoCRM.model.Project;
 import ru.javamentor.EcoCRM.model.StepNumber;
@@ -8,12 +7,9 @@ import ru.javamentor.EcoCRM.model.StepNumber;
 @Service
 public class ProjectService {
 
-    @Autowired
-    StepService stepService;
-
     public void addProject(Project project) {
         for (StepNumber step : StepNumber.values()) {
-            stepService.addStep(step, project);
+
         }
     }
 }

8894101e30c171c5cde95f28fc421d1abc035164
NassyMyth12 <nastia.mefyod@yandex.ru>
2019-08-20 20:00:48 +0300

    user list for admin

--- src/main/java/ru/javamentor/EcoCRM/controller/UsersListController.java
+++ src/main/java/ru/javamentor/EcoCRM/controller/UsersListController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import ru.javamentor.EcoCRM.model.User;
 import ru.javamentor.EcoCRM.service.UserService;
 
@@ -20,18 +17,11 @@ public class UsersListController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public String showAllUsers(Model model) {
-          List<User> listusers = userService.getAll();
-            System.out.println(listusers.toString());
-          model.addAttribute("listusers", listusers);
-//        ModelAndView modelAndView = new ModelAndView("user");
-//        modelAndView.addObject("user.name", user);
-//        modelAndView.addObject("user.mail", user);
-//        modelAndView.addObject("user.status", user);
-//        return modelAndView;
-
-        System.out.println("controller ready");
-        return "users";
+          List<User> usersList = userService.getAll();
+          System.out.println(usersList.toString());
+          model.addAttribute("usersList", usersList);
+        return "/admin/users";
     }
 }

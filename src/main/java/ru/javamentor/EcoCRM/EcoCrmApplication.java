07e0ff348b8036d6b88061708a4d044960f57882
Kirillabaskin <kirillabaskin99@gmail.com>
2019-08-20 21:34:53 +0300

    vk registration with example form html and controller

--- src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
+++ src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
@@ -1,9 +1,16 @@
 package ru.javamentor.EcoCRM;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +18,9 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import ru.javamentor.EcoCRM.init.DataInitializer;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class EcoCrmApplication implements WebMvcConfigurer {
@@ -21,7 +31,7 @@ public class EcoCrmApplication implements WebMvcConfigurer {
 		return new DataInitializer();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, JSONException {
 		SpringApplication.run(EcoCrmApplication.class, args);
 	}
 

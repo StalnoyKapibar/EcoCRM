8060528fcc05a3ab08b0e5bad0e9fdeecef37740
Marfall <marinefalling@gmail.com>
2019-08-20 14:55:37 +0300

    mail send with service and form

--- src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
+++ src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
@@ -12,11 +12,13 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import ru.javamentor.EcoCRM.init.DataInitializer;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class EcoCrmApplication implements WebMvcConfigurer {
 
-//	@PostConstruct
-//	@Bean(initMethod = "init")
+	@PostConstruct
+	@Bean(initMethod = "init")
 	public DataInitializer init() {
 		return new DataInitializer();
 	}
--- src/main/java/ru/javamentor/EcoCRM/controller/SendRegistrationController.java
+++ src/main/java/ru/javamentor/EcoCRM/controller/SendRegistrationController.java
@@ -1,7 +1,32 @@
 package ru.javamentor.EcoCRM.controller;
 
-/**
- * Created by whitenoise on 20.08.19.
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.javamentor.EcoCRM.service.EmailServiceImpl;
+
+@Controller
 public class SendRegistrationController {
+
+    @Autowired
+    private EmailServiceImpl emailServiceimp;
+
+    @GetMapping("/sendReg")
+    public String SendReg(Model model) {
+        String userEmail = "";
+        model.addAttribute("userEmail",userEmail);
+        return "sendregform";
+    }
+    @PostMapping("/processSendForm")
+   // public String processSendForm(@RequestParam(name = "userEmail") String userEmail) {
+    public String   processingSenForm(@ModelAttribute("userEmail") String userEmail) {
+        System.out.println("user email is: " + userEmail);
+        emailServiceimp.sendSimpleMessage(userEmail,"To target mail from form", "Hello,Volunteer! Welcome to our Service!");
+        System.out.println("mailsended");
+        return "admin_page";
+    }
 }

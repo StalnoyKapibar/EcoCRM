1242a1a441342e8b1ed1ffb2cd86b8b2659ac124
Denis Erkenov <derkenov@inbox.ru>
2019-08-22 15:35:04 +0300

    Merge branch 'dev' of https://github.com/StalnoyKapibar/EcoCRM into TaskComment
    # Conflicts:
    #	src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java

--- src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
+++ src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java
@@ -3,18 +3,17 @@ package ru.javamentor.EcoCRM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.javamentor.EcoCRM.init.DataInitializer;
 
-import javax.annotation.PostConstruct;
+
 
 @SpringBootApplication
 public class EcoCrmApplication implements WebMvcConfigurer {
-
-	@PostConstruct
-	@Bean(initMethod = "init")
+//
+//	@PostConstruct
+//	@Bean(initMethod = "init")
 	public DataInitializer init() {
 		return new DataInitializer();
 	}

f80023125710c5123f80170e5e74b224226cb783
Denis Erkenov <derkenov@inbox.ru>
2019-08-24 01:12:12 +0300

    Merge branch 'dev' of https://github.com/StalnoyKapibar/EcoCRM into TaskComment
    # Conflicts:
    #	src/main/java/ru/javamentor/EcoCRM/EcoCrmApplication.java

--- src/main/resources/static/private/js/petitionForAdminWithUser.js
+++ src/main/resources/static/private/js/petitionForAdminWithUser.js
@@ -10,10 +10,16 @@ function f() {
         success: function (petition) {
             var template = '';
             var name;
-            
+            var surname;
+            var email;
+            var  phone;
+
             petition.forEach(petition => {
                 petition.userList.forEach(user=>{
                      name = user.name;
+                     surname = user.surname;
+                     email = user.email;
+                     phone = user.phone;
 
 
                 });
@@ -21,12 +27,15 @@ function f() {
                template+='<div class="col-sm-4">' +
                    '<div class="card shadow bg-white rounded" style="margin-top: 15px">' +
                    '<div class="card-header">Заявка</div>' +
-                '<div class="card-body">Дата поступления заявки:' +
+                   '<div class="card-body">Дата поступления заявки:' +
                    '<h5 id="2">'+ petition.data +'' +
                    '</h5>Адрес:<h5 id="3">'+ petition.adress + '' +
                    '</h5> Район:<h5 id="4">' + petition.area + '' +
                    '</h5>Имя юзера<h5 id="5">' + name + '' +
-                   '</h5> <button>Подать заявку</button> </div></div></div>'
+                   '</h5>Фамилия<h5 id="6">' + surname + '' +
+                   '</h5>Емаил<h5 id="7">' + email + ''+
+                   '</h5>Номер телефона<h5 id="8">' + phone + ''+ '</h5>'+
+                   '<button>Одобрить заявку</button> </div></div></div>'
                 $("#cardes").html(template);
             });
         }

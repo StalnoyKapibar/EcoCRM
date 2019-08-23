58cbe64eebe9d7a28b553a24d62884005f4091d0
Alexey Fomichev <hang232@yandex.ru>
2019-08-23 17:24:18 +0300

    sorry for working on dev v.2

--- src/main/resources/static/private/js/projects.js
+++ src/main/resources/static/private/js/projects.js
@@ -7,13 +7,13 @@ function fillContent() {
             let count = 0;
             let docVar = '';
             $.each(projects, function (key, value) {
-                docVar += '<div class="col-2">' +
+                docVar += '<div class="col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2">' +
                     '                <div class="eco-column-header-extras">' +
                     '                    <h6>Шаг ' + (count+1) + '</h6>' +
                     '                </div>' +
                     '            <div class="eco-col card" id="col_' + count + '" style="height:47rem;overflow:auto;background-color: #F5F5F5">';
                 $.each(value, function (i, project) {
-                    docVar += '<div class="eco-card card m-2" style="height: 20%;box-shadow: 0px 5px 9px -8px #000000;">' +
+                    docVar += '<div class="eco-card card m-2" style="height:20%;box-shadow: 0px 5px 9px -8px #000000;">' +
                         '                            <div class="card-header" style="font-size: small">' + project.title + '</div>' +
                         '                            <div class="card-body">' +
                         '                            <div class="house-area">' + project.petition.houseArea + '</div>' +

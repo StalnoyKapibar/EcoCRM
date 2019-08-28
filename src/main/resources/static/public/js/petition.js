2a58065adb56a249711b6226b41befb8145ac1f6
Alexey Fomichev <hang232@yandex.ru>
2019-08-29 00:40:07 +0300

    added animation to petition.html

--- src/main/resources/static/public/js/petition.js
+++ src/main/resources/static/public/js/petition.js
@@ -1,22 +1,42 @@
-// function addUserToPetition(id) {
-//     $.ajax({
-//         type: 'put',
-//         url: '/api/petition/addPetitionUser?id='+id,
-//         async: false,
-//         success:function () {
-//             alert("ВЫ ДОБАВИЛИ ЧТО ТО ТАМ")
-//             $('#'+id+'_remove_btn').remove("");
-//             },
-//     });
-// }
+const statusInfo = 'alert_header';
 
-
-let sendPetition = function(event) {
-
+let scrollTop = function() {
+    $("body,html").animate({
+        scrollTop:0
+    }, 800);
+    return false;
 };
 
-// var header = $("meta[name='_csrf_header']").attr("content");
-// var token = $("meta[name='_csrf']").attr("content");
+let alertInfo = function(state) {
+    let form = $('#petition_form');
+    if(form.children('#'+statusInfo).length > 0) {
+        $('#'+statusInfo).detach();
+    }
+    switch (state) {
+        case 'success':
+            $(form).prepend('<div class="alert alert-success" role="alert" id="'+statusInfo+
+                '">\n' +
+                '  Заявка отправлена!\n' +
+                '</div>');
+            break;
+        case 'wait':
+            $(form).prepend('<div class="alert alert-warning" role="alert" id="'+statusInfo+
+                '">\n' +
+                '  Ваша заявка обрабатывается. Пожалуйста, подождите.\n' +
+                '</div>');
+            break;
+        case 'error':
+            $(form).prepend('<div class="alert alert-danger" role="alert" id="alert_header' +
+                '">\n' +
+                '  При обработке заявки произошла ошибка!\n' +
+                '</div>');
+            break;
+    }
+    if(state === 'success') {
+
+    }
+}
+
 $(document).ready(function() {
     $('#petition_form').submit(function (event) {
         let row_types = $('form input[name=typeOfRawMaterial]:checked').map(function () {
@@ -50,14 +70,18 @@ $(document).ready(function() {
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
             data: JSON.stringify(petition),
-            success: function () {
-                console.log('asdasd');
+            success: function (response) {
+                console.log(response.status);
+                alertInfo('success');
             },
-            error: function (e) {
-                console.log(e);
+            error: function (response) {
+                alertInfo('error');
             }
         });
         event.preventDefault();
+        $('input').attr('disabled', true);
+        alertInfo('wait');
+        scrollTop();
     });
 });
 
